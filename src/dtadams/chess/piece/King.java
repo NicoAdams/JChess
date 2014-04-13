56608c7152f684ac70f80c2d5010eb352f393c99
Dominic Adams <dtadams@Dominics-MacBook-Pro.local>
2014-04-12 21:12:15 -0400

    Fixed check rules -- cannot castle out of/through check
    Game rules are now fully implemented and functionald

--- src/dtadams/chess/piece/King.java
+++ src/dtadams/chess/piece/King.java
@@ -25,21 +25,10 @@ public class King extends Piece {
 		return this.hasMoved;
 	}
 
-	public ArrayList<Position> getMoveListIgnoreCheck(Position current, Board b) {
-		int[] rowDeltas = new int[]{1, 0, -1, -1, -1, 0, 1, 1};
-		int[] colDeltas = new int[]{1, 1, 1, 0, -1, -1, -1, 0};
-
-		ArrayList<Position> moveList = Mover.getMoves(current,
-														b,
-														rowDeltas,
-														colDeltas,
-														color,
-														Mover.Type.SINGLE,
-														Mover.Capture.ALLOWED);
+	public ArrayList<Position> getMoveList(Position current, Board b) {
+		ArrayList<Position> moveList = super.getMoveList(current, b);
 
 		// Castling
-		// KEY TO SOLVING CASTLING CHECK CHECKING: Move this to getMoveList
-
 		if(!this.hasMoved) {
 
 			ArrayList<Position> rookPositions = b.locatePiece(PieceType.ROOK, color);
@@ -59,6 +48,20 @@ public class King extends Piece {
 		}
 
 		return moveList;
+
+	} 
+
+	public ArrayList<Position> getMoveListIgnoreCheck(Position current, Board b) {
+		int[] rowDeltas = new int[]{1, 0, -1, -1, -1, 0, 1, 1};
+		int[] colDeltas = new int[]{1, 1, 1, 0, -1, -1, -1, 0};
+
+		return Mover.getMoves(current,
+								b,
+								rowDeltas,
+								colDeltas,
+								color,
+								Mover.Type.SINGLE,
+								Mover.Capture.ALLOWED);
 	}
 
 	public Piece copy() {
@@ -83,18 +86,17 @@ public class King extends Piece {
 
 		int row = current.row;
 		int delta = (int) Math.signum(pos.col - current.col);
-		for(int col=current.col+delta; delta*col < delta*pos.col; col+=delta) {
+		for(int col=current.col; delta*col < delta*pos.col; col+=delta) {
 
 			Position testPos = new Position(row, col);
 
 			if(!b.getPiece(testPos).isNone()) return false;
 
-			// Checks for check in each space
-			// TODO
-			// Board b1 = b.copy();
-			// b1.setPiece(current, new NonePiece());
-			// b1.setPiece(testPos, this);
-			// if(b1.inCheck(this.getColor())) return false;
+//			Checks for check in each space
+			Board b1 = b.copy();
+			b1.setPiece(current, new NonePiece());
+			b1.setPiece(testPos, this);
+			if(b1.inCheck(this.getColor())) return false;
 		}
 
 		return true;
